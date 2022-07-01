895953bc2372758c8a4fb38f43ad7a5dc6b1a3a9
Freem11 <freem1985@gmail.com>
2022-06-30 23:30:19 -0600

    custom marker code added anchors for now

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { data } from "./data/testdata";
+import anchor from '../images/anchor6.png'
+import L from 'leaflet';
 
 export default function Map({ searchParams }) {
   const [latitude, setLatitude] = useState(49.246292);
   const [longitude, setLongitude] = useState(-123.116226);
 
   const locations = data.filter((loc) => loc.location === "vancouver");
+
+  let anchorIcon = L.icon({
+    iconUrl: anchor,
+    iconRetinaUrl: anchor,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 25],
+  });
 
   console.log("Locations:", locations);
 
@@ -16,6 +26,7 @@ export default function Map({ searchParams }) {
       setLongitude(location.coords.longitude);
     });
   }, [latitude, longitude]);
+
   const initialPosition = [latitude, longitude];
   const [position, setPosition] = useState(initialPosition);
 
@@ -34,9 +45,9 @@ export default function Map({ searchParams }) {
 
         {locations &&
           locations[0].sites.map((dataLoc) => (
-            <Marker position={[dataLoc.lat, dataLoc.long]} key={dataLoc.lat}>
+            <Marker position={[dataLoc.lat, dataLoc.long]} key={dataLoc.lat} icon={anchorIcon}>
               <Popup>
-                Enjoy Scuba Diving at <br /> {dataLoc}
+                Enjoy Scuba Diving at <br /> {dataLoc.lat + "," + dataLoc.long}
               </Popup>
             </Marker>
           ))}

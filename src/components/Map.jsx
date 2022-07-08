e8aefa27cd46d5821b856296a159487e9402448a
freem11 <freem1985@gmail.com>
2022-07-08 12:58:15 -0600

    current map center GPS coords and zoom level now tracked

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { data, backups, tester } from "./data/testdata";
 import anchor from "../images/anchor6.png";
 import { HeatmapLayer } from "react-leaflet-heatmap-layer-v3";
 import L from "leaflet";
 
-export default function Map({ searchParams }) {
-  const [latitude, setLatitude] = useState(49.246292);
-  const [longitude, setLongitude] = useState(-123.116226);
+const center = [49.246292, -123.116226]
+const zoom = 7
+let timoutHanlder;
+
+function DisplayPosition({map}) {
+
+  const [position, setPosition] = useState(() => map.getCenter())
+  const [zoomlev, setZoomlev] = useState(() => map.getZoom())
+
+  const onMove = useCallback(() => {
+    window.clearTimeout(timoutHanlder)
+    timoutHanlder = window.setTimeout(function(){
+      setPosition(map.getCenter()) 
+      setZoomlev(map.getZoom())
+    },50)
+    },
+    [map],
+  )
+
+  useEffect(() => {
+      map.on('move', onMove)
+    return () => {
+      map.off('move', onMove)
+    }
+  }, [map, onMove])
+
+  return(<div style={{zIndex: 7, position: "relative", top: '300px'}}>{position.lat}, {position.lng} {zoomlev}</div>)
+}
+
+ function ExtState({ searchParams }) {
+  const [map , setMap] = useState(null);
 
   const locations = data.filter((loc) => loc.location === "vancouver");
 
@@ -20,57 +48,67 @@ export default function Map({ searchParams }) {
   });
 
   const gradient1 = { 0.2: "blue", 0.4: "green", 0.6: "yellow", 0.8: "orange", 1: "red"};
-  const gradient2 = { 0.4: "red", 0.8: "red", 1.0: "red" };
-  const gradient3 = { 0.4: "green", 0.8: "green", 1.0: "green" };
-  const gradient4 = { 0.4: "orange", 0.8: "orange", 1.0: "orange" };
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (location) {
-      setLatitude(location.coords.latitude);
-      setLongitude(location.coords.longitude);
-    });
-  }, [latitude, longitude]);
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(function (location) {
+  //     setLatitude(location.coords.latitude);
+  //     setLongitude(location.coords.longitude);
+  //   });
+  // }, [latitude, longitude]);
 
-  const initialPosition = [latitude, longitude];
-  const [position, setPosition] = useState(initialPosition);
+  // const initialPosition = [latitude, longitude];
+  // const [position, setPosition] = useState(initialPosition);
 
+
+  const displayMap = useMemo(() => (
+  
+  <MapContainer
+    center={center}
+    zoom={zoom}
+    scrollWheelZoom={true}
+    ref={setMap}
+  >
+    <HeatmapLayer
+      fitBoundsOnLoad
+      fitBoundsOnUpdate
+      points={tester}
+      longitudeExtractor={(m) => m[1]}
+      latitudeExtractor={(m) => m[0]}
+      intensityExtractor={(m) => parseFloat(m[2])}
+      radius={10}
+      max={1}
+      minOpacity={1}
+      gradient={gradient1}
+    />
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+
+    {locations &&
+      locations[0].sites.map((dataLoc) => (
+        <Marker
+          key={dataLoc.key}
+          position={[dataLoc.lat, dataLoc.long]}
+          icon={anchorIcon}
+        >
+          <Popup>
+            Enjoy Scuba Diving at <br /> {dataLoc.lat + "," + dataLoc.long}
+          </Popup>
+        </Marker>
+      ))}
+  </MapContainer>), [])
+  
   return (
     <div className="map-container">
-      <MapContainer
-        center={position && position}
-        zoom={7}
-        scrollWheelZoom={true}
-      >
-        <HeatmapLayer
-          fitBoundsOnLoad
-          fitBoundsOnUpdate
-          points={tester}
-          longitudeExtractor={(m) => m[1]}
-          latitudeExtractor={(m) => m[0]}
-          intensityExtractor={(m) => parseFloat(m[2])}
-          radius={10}
-          max={1}
-          minOpacity={1}
-          gradient={gradient1}
-        />
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        {locations &&
-          locations[0].sites.map((dataLoc) => (
-            <Marker
-              key={dataLoc.key}
-              position={[dataLoc.lat, dataLoc.long]}
-              icon={anchorIcon}
-            >
-              <Popup>
-                Enjoy Scuba Diving at <br /> {dataLoc.lat + "," + dataLoc.long}
-              </Popup>
-            </Marker>
-          ))}
-      </MapContainer>
+      {map ? <DisplayPosition map={map} /> : null}
+      {displayMap}
     </div>
   );
+}
+
+export default function () {
+  return (
+    <ExtState />
+  )
 }
